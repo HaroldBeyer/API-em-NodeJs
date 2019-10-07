4740fa34e7844f6013cde095c5f47ac28ab251d6
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-07 14:44:21 -0300

    NormalizePort

--- server.js
+++ server.js
@@ -21,3 +21,14 @@ app.use('/', route);
 server.listen(port);
 
 console.log("API rodando na porta " + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port))
+        return val;
+
+    if (port >= 0)
+        return port;
+
+    return false;
+}
