32d1321dfca3f5ca4913915a3747b741355e3793
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-07 15:16:56 -0300

    Separando o servidor da aplicação

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,13 @@
 'use strict'
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
+const app = require('../src/app')
 
-const app = express();
+
 const port = normalizePort(process.env.PORT || '9610');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-var route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Teste",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
