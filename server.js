e72a78ffcc7aa5d0ac5cb62002ace2d9c9f821ce
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-07 14:42:01 -0300

    Criando servidor web

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 'use strict'
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
 
-console.log("Testando...");
+const app = express();
+const port = 9610;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+var route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Teste",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+server.listen(port);
+
+console.log("API rodando na porta " + port);
