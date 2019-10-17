662edd8e24aa03b7c17c3fec155e162d6ee42360
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-17 16:02:31 -0300

    Listando produto pelo slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,38 +1,56 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
-const Product = mongoose.model('Product');
+const mongoose = require("mongoose");
+const Product = mongoose.model("Product");
 
 exports.get = (req, res, next) => {
-    Product.find({}, 'title price slug').then(data => {
-        res.status(200).send(data);
-    }).catch(error => {
-        res.status(400).send(error);
+  Product.find({}, "title price slug")
+    .then(data => {
+      res.status(200).send(data);
     })
-}
+    .catch(error => {
+      res.status(400).send(error);
+    });
+};
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    { slug: req.params.slug, active: true },
+    "title description price slug tags"
+  )
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    });
+};
 
 exports.post = (req, res, next) => {
-    const product = new Product(req.body);
-    product.save().then(x => {
-        res.status(200).send({
-            message: "Produto cadastrado com sucesso"
-        })
-    }).catch(error => {
-        res.status(400).send({
-            message: "Falha ao cadastrar o produto",
-            data: error
-        })
+  const product = new Product(req.body);
+  product
+    .save()
+    .then(x => {
+      res.status(200).send({
+        message: "Produto cadastrado com sucesso"
+      });
     })
+    .catch(error => {
+      res.status(400).send({
+        message: "Falha ao cadastrar o produto",
+        data: error
+      });
+    });
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-}
+  const id = req.params.id;
+  res.status(201).send({
+    id: id,
+    item: req.body
+  });
+};
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
-}
+  res.status(200).send(req.body);
+};
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/product-controller');
+const controller = require("../controllers/product-controller");
 
-router.post('/', controller.post);
+router.post("/", controller.post);
 
-router.put('/:id', controller.put);
+router.put("/:id", controller.put);
 
-router.delete('/', controller.delete);
+router.delete("/", controller.delete);
 
-router.get('/', controller.get);
+router.get("/", controller.get);
+
+router.get("/:slug", controller.getBySlug);
 
 module.exports = router;
